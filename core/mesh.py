29fdff0404fe1e417e4b3451234589e045f8d991
manoj <malviyamanoj1896@gmail.com>
2026-01-27 20:51:06 +0100

    nit

---
+++ core/mesh.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def plot_mesh(matrix: np.ndarray, material: np.ndarray):
+    """
+    from matplotlib import pyplot as plt
+    Plot the pixel meshgrid.
+    Parameters:
+    """
